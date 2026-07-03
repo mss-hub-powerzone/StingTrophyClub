af2766fb19ac1bdae068a2189e5d61984ad41e17
Jon Barber <jon@stingtrophyclub>
2026-07-03 04:03:22 +0000

    feat: U17 kit numbers

---
+++ migrations/0006_u17_kit_numbers.sql
@@ -0,0 +1,16 @@
+-- U17 kit number updates from roster screenshots (July 2026)
+UPDATE players SET kit_number='24'  WHERE id='aidan-martin-2010-07-07';
+UPDATE players SET kit_number='2'   WHERE id='emilio-gutierrez-2010-02-14';
+UPDATE players SET kit_number='99'  WHERE id='evan-john-2010-07-30';
+UPDATE players SET kit_number='14'  WHERE id='hunter-graves-2010-05-03';
+UPDATE players SET kit_number='18'  WHERE id='keaton-lucero-39';
+UPDATE players SET kit_number='20'  WHERE id='kellen-keahey-2010-01-19';
+UPDATE players SET kit_number='4'   WHERE id='kyan-barber-2010-01-29';
+UPDATE players SET kit_number='00'  WHERE id='landon-prichard-2010-04-17';
+UPDATE players SET kit_number='6'   WHERE id='lucas-armbruster-2010-05-08';
+UPDATE players SET kit_number='16'  WHERE id='new-player-zqwlb4z';
+UPDATE players SET kit_number='88'  WHERE id='nolan-wankowicz-37';
+UPDATE players SET kit_number='22'  WHERE id='preston-overfield-2010-05-20';
+UPDATE players SET kit_number='0'   WHERE id='robert-kawaka-2010-05-25';
+UPDATE players SET kit_number='8'   WHERE id='ryder-mathine-2010-07-22';
+UPDATE players SET kit_number='10'  WHERE id='zach-morris-2010-02-12';
